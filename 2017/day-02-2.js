a5dd940ced7c61117f6a5e5be072c17148f6d52e
Ramuns Usovs <ramuns.usovs@booking.com>
2017-12-05 11:52:43 +0100

    Minor optimization to part two of day 2

--- 2017/day-02-2.js
+++ 2017/day-02-2.js
@@ -13,6 +13,19 @@ console.log(
             do {
                 divisor = ints.shift();
                 for ( var i = ints.length - 1; i >= 0; i-- ) {
+                    if ( ( ints[i] / divisor ) < 2 ) {
+                        // in this case if the current number / divisor < 2 that means
+                        // that there's only one case where we can have a divisor/dividend === integer
+                        // that's if there's a number that equals to the divisor in the array
+                        // since the array is sorted then that number has to be the first number in there
+                        // so if it's there we say we found it, otherwise we need to take the next candidate
+                        // divisor anyway
+                        if ( ints[0] === divisor ) {
+                            dividend = ints[0];
+                            found = true;
+                        }
+                        break;
+                    }
                     if ( ints[i] % divisor === 0 ) {
                         found = true;
                         dividend = ints[i];
