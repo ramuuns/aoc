9017dc1adb8a4964c45a1fdb9fc33adecfa66b2f
Ramūns <ramuuns@enkurs.org>
2018-12-01 14:40:34 +0100

    Minor fuckery with 1-2
    Make it possible to deal with input files that don't have a new line
    at the end. As well as add a callback function to the get_input, so
    that we can return a vector of _parsed_ stuff, instead of strings,
    this way we parse the strings to integers only once, instead of every
    time.
    
    Also use a HashSet as it's more appropriate

--- 2018/1-2.rs
+++ 2018/1-2.rs
@@ -1,13 +1,14 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 /*
-    Read a file given by a filename and parse it to a vector of strings (one per line)
+    Read a file given by a filename and parse it to a vector of
+    whatever we need given a line_transform function
 */
-fn get_input(filename : String) -> Vec<String> {
+fn get_input<T>(filename : &str, line_transform : fn(String) -> T) -> Vec<T> {
     let mut file = File::open(filename).expect("Cannot open file");
-    let mut ret : Vec<String> = Vec::new();
+    let mut ret : Vec<T> = Vec::new();
     let mut vec_one_line = Vec::new();
     loop {
         let mut buf = [0;512];
@@ -15,6 +16,12 @@ fn get_input(filename : String) -> Vec<String> {
         match res {
             Ok(n) => {
                 if n == 0 {
+                    // If the file doesn't end on a new line, we'll have stuff
+                    // in our "current line" vector, thus we need to parse it as well
+                    if vec_one_line.len() > 0 {
+                        let s = String::from_utf8(vec_one_line.clone()).unwrap();
+                        ret.push(line_transform(s));
+                    }
                     break;
                 }
 
@@ -22,9 +29,14 @@ fn get_input(filename : String) -> Vec<String> {
                     if *b == 10 {
                         let s = String::from_utf8(vec_one_line.clone()).unwrap();
                         vec_one_line.clear();
-                        ret.push(s);
+                        ret.push(line_transform(s));
                     } else {
-                        vec_one_line.push(*b);
+                        // deal with null bytes, that are in the buffer ('cause it's fixed size), when
+                        // we're at the end of the file. Since we're reading a text file those shouldn't be in
+                        // legitimate places, so they're safe to ignore
+                        if *b > 0 {
+                            vec_one_line.push(*b);
+                        }
                     }
                 }
             }
@@ -38,18 +50,18 @@ fn get_input(filename : String) -> Vec<String> {
 }
 
 fn main()  {
-    let input = get_input("input-1".to_string());
+    let input = get_input("input-1", |s| s.parse::<i32>().unwrap());
     let mut sum = 0;
-    let mut seen_hash : HashMap<i32,i32> = HashMap::new();
+    let mut seen_hash = HashSet::new();
     let mut found = false;
     loop {
         for line in &input {
-            sum += line.parse::<i32>().unwrap();
-            if seen_hash.contains_key(&sum) {
+            sum += line;
+            if seen_hash.contains(&sum) {
                 found = true;
                 break;
             }
-            seen_hash.insert(sum,1);
+            seen_hash.insert(sum);
         }
         if found {
             break;
