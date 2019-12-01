cde8dd76e9a761de4f6dab7513e6ac3b0bc777ef
Ramūns <ramuuns@enkurs.org>
2019-12-01 09:10:41 +0100

    Day 1 of 2019
    This year I'm doing this while teaching myself golang

---
+++ 2019/day-01-1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"strconv"
+)
+
+func main () {
+	input, err := ioutil.ReadFile("input-01")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	input_as_str := string(input)
+	data := strings.Split(input_as_str, "\n")
+	var sum = 0
+	for _, val := range(data) {
+		intval, err := strconv.Atoi(val)
+		if err != nil {
+			//meh
+		} else {
+			sum += intval/3 - 2
+		}
+	}
+	fmt.Println(sum)
+}
---
+++ 2019/day-01-2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"strconv"
+)
+
+func main () {
+	input, err := ioutil.ReadFile("input-01")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	input_as_str := string(input)
+	data := strings.Split(input_as_str, "\n")
+	var sum = 0
+	for _, val := range(data) {
+		mweight, err := strconv.Atoi(val)
+		if err != nil {
+			//meh
+		} else {
+			var mweight_fuel = mweight/3 - 2
+			var tmp = mweight_fuel
+			var tmp_sum = 0
+			for tmp > 0 {
+				tmp_sum += tmp
+				tmp = tmp / 3 - 2
+			}
+			sum += tmp_sum
+		}
+	}
+	fmt.Println(sum)
+}
