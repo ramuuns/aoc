62f73deb021862bc181c29319c518ef64e5c7bce
Ramuns Usovs <ramuns.usovs@booking.com>
2019-12-16 12:13:36 +0100

    Day 16
    damn this was hard

--- 2019/day-16-1.go
+++ 2019/day-16-1.go
@@ -1,11 +1,11 @@
 package main
-  
+
 import (
-    "fmt"
-//    "gopkg.in/karalabe/cookiejar.v1/collections/deque"
-    "io/ioutil"
-    "strconv"
-    "strings"
+	"fmt"
+	//    "gopkg.in/karalabe/cookiejar.v1/collections/deque"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 func abs(n int) int {
@@ -17,37 +17,36 @@ func abs(n int) int {
 
 func main() {
 	input, err := ioutil.ReadFile("input-16")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    input_as_str := string(input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	input_as_str := string(input)
 	signal_as_chars := strings.Split(strings.TrimSpace(input_as_str), "")
-	signal := make([]int,len(signal_as_chars));
+	signal := make([]int, len(signal_as_chars))
 	for i, cv := range signal_as_chars {
 		signal[i], _ = strconv.Atoi(cv)
 	}
 
-	var pattern = [4]int{0, 1, 0, -1};
+	var pattern = [4]int{0, 1, 0, -1}
 
 	for k := 0; k < 100; k++ {
-		new_signal :=  make([]int,len(signal));
+		new_signal := make([]int, len(signal))
 		for i, _ := range signal {
 			var it = 0
 			for c := i; c < len(signal); c++ {
-			//	fmt.Print(" + ", pattern[( (c+1)/(i+1) )%4] ," * ", signal[c])
-				it += pattern[( (c+1)/(i+1) +4)%4] * signal[c]
+				//	fmt.Print(" + ", pattern[( (c+1)/(i+1) )%4] ," * ", signal[c])
+				it += pattern[((c+1)/(i+1)+4)%4] * signal[c]
 			}
 			//fmt.Println(" = ", abs(it%10));
-			new_signal[i] = abs(it%10);
+			new_signal[i] = abs(it % 10)
 		}
 		//fmt.Println(new_signal)
 		signal = new_signal
 	}
 
-	for c := 0; c<8; c++ {
+	for c := 0; c < 8; c++ {
 		fmt.Print(signal[c])
 	}
 	fmt.Println()
 }
-
--- 2019/day-16-2.go
+++ 2019/day-16-2.go
@@ -1,11 +1,11 @@
 package main
-  
+
 import (
-    "fmt"
-//    "gopkg.in/karalabe/cookiejar.v1/collections/deque"
-    "io/ioutil"
-    "strconv"
-    "strings"
+	"fmt"
+	//    "gopkg.in/karalabe/cookiejar.v1/collections/deque"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 func abs(n int) int {
@@ -17,48 +17,53 @@ func abs(n int) int {
 
 func main() {
 	input, err := ioutil.ReadFile("input-16")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    input_as_str := string(input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	input_as_str := string(input)
 	signal_as_chars := strings.Split(strings.TrimSpace(input_as_str), "")
-	signal := make([]int,len(signal_as_chars)*10000);
+	period := 10000
+	signal := make([]int, len(signal_as_chars)*period)
 	for i, cv := range signal_as_chars {
-		v, _ := strconv.Atoi(cv);
-		for k := 0; k < 10000; k++ {
+		v, _ := strconv.Atoi(cv)
+		for k := 0; k < period; k++ {
 			signal[i+k*len(signal_as_chars)] = v
 		}
 	}
 
 	var offset int
-	for c := 0; c<7; c++ {
+	for c := 0; c < 7; c++ {
 		offset *= 10
 		offset += signal[c]
 	}
-	fmt.Println(offset);
+	fmt.Println(offset)
+	signal = signal[offset:]
+	fmt.Println(len(signal))
 
-	var pattern = [4]int{0, 1, 0, -1};
+	//var pattern = [4]int{0, 1, 0, -1};
 
 	for k := 0; k < 100; k++ {
-		fmt.Println(k)
-		new_signal :=  make([]int,len(signal));
+		//		fmt.Println(k)
+		new_signal := make([]int, len(signal))
 		for i, _ := range signal {
-			fmt.Println(i)
+			//fmt.Println(i)
 			var it = 0
-			for c := i; c < len(signal); c++ {
-				if pattern[( (c+1)/(i+1) +4)%4] != 0 {
-					it += pattern[( (c+1)/(i+1) +4)%4] * signal[c]
+			if i == 0 {
+				for c := i; c < len(signal); c++ {
+					it += signal[c]
 				}
+				new_signal[i] = abs(it % 10)
+			} else {
+				new_signal[i] = (new_signal[i-1] + 10 - signal[i-1]) % 10
 			}
-			new_signal[i] = abs(it%10);
 		}
+		//fmt.Println(new_signal);
 		signal = new_signal
 	}
 
-	for c := offset; c<8; c++ {
+	for c := 0; c < 8; c++ {
 		fmt.Print(signal[c])
 	}
 	fmt.Println()
 }
-
