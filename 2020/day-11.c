3fb4a09a17f4215ce9515ac255cc13732ef668ee
Ramūns <ramuuns@enkurs.org>
2020-12-11 16:52:36 +0100

    Day 11 doing both parts in the same main loop
    annoyingly this has not yielded the improvements that I hoped, it is
    faster, but only marginally.

--- 2020/day-11.c
+++ 2020/day-11.c
@@ -51,146 +51,40 @@ int main() {
     fclose(fp);
     printtime();
     char *grid_p2 = malloc(sizeof (char) * (rows+2) * (rowsize+2) );
-    strncpy(grid_p2, grid, (rows+2) * (rowsize+2));
     char *p_grid = malloc(sizeof (char) * (rows+2) * (rowsize+2) );
-    char *fixed_seats = calloc((rows+2) * (rowsize+2), sizeof(char)); 
+    char *p_grid_p2 = malloc(sizeof (char) * (rows+2) * (rowsize+2) );
+    strncpy(grid_p2, grid, (rows+2) * (rowsize+2));
+    strncpy(p_grid_p2, grid, (rows+2) * (rowsize+2));
     strncpy(p_grid, grid, (rows+2) * (rowsize+2));
-    int has_changed = 0;
-    char **grd = &grid;
-    char **pgrid = &p_grid;
+    char *fixed_seats = calloc((rows+2) * (rowsize+2), sizeof(char)); 
+    char *fixed_seats_p2 = calloc((rows+2) * (rowsize+2), sizeof(char)); 
+    int has_changed_p1 = 0;
+    int has_changed_p2 = 0;
+    int p1_done = 0;
+    int p2_done = 0;
+    char **grdp1 = &grid;
+    char **pgridp1 = &p_grid;
+    char **grdp2 = &grid_p2;
+    char **pgridp2 = &p_grid_p2;
 
     int minx = 1;
     int miny = 1;
     int maxx = rowsize;
     int maxy = rows;
 
-    do {
-        char **tmp = grd;
-        grd = pgrid;
-        pgrid = tmp;
-
-        //print_grid(*pgrid, rowsize+2, rows+2);
-//        iter++;
-//        if (iter > 6 ) {
-//           return 1;
-//        }
-
-        int thisminx = maxx;
-        int thismaxx = minx;
-        int thisminy = maxy;
-        int thismaxy = miny;
-
-        has_changed = 0;
-        for ( int y = miny; y <= maxy; y++ ) {
-            for ( int x = minx; x <= maxx; x++ ) {
-                if ( *(fixed_seats +  y*(rowsize+2) + x) ) {
-                    continue;
-                }
-                if ( *((*pgrid) + y*(rowsize+2) + x ) == 'L' ) {
-                    *((*grd) + y*(rowsize+2) + x ) = *((*pgrid) + y*(rowsize+2) + x );
-                    
-                    for ( int yd = -1; yd < 2; yd++ ) {
-                        for ( int xd = -1; xd < 2; xd++ ) {
-                            if ( yd == 0 && xd == 0 ) {
-                                continue;
-                            }
-                            if ( OCCUPIED( *((*pgrid) + (y+yd)*(rowsize+2) + x +xd ) ) ) {
-                                goto occ1;
-                            }
-                        }
-                    }
-
-                    *((*grd) + y*(rowsize+2) + x ) = '#';
-                    has_changed = 1;
-                    if ( x > thismaxx ) {
-                       thismaxx = x;
-                    }
-                    if ( y > thismaxy ) {
-                       thismaxy = y;
-                    }
-                    if ( x < thisminx ) {
-                       thisminx = x;
-                    }
-                    if ( y < thisminy ) {
-                       thisminy = y;
-                    }
-                    continue;
-occ1:
-                    *(fixed_seats + + y*(rowsize+2) + x) = 1;
-                } else if ( *((*pgrid) + y*(rowsize+2) + x ) == '#' ) {
-                    *((*grd) + y*(rowsize+2) + x ) = *((*pgrid) + y*(rowsize+2) + x );
-                    int occ_count = 0;
-                    for ( int yd = -1; yd < 2; yd++ ) {
-                        for ( int xd = -1; xd < 2; xd++ ) {
-                            if ( yd == 0 && xd == 0 ) {
-                                continue;
-                            }
-                            if ( OCCUPIED( *((*pgrid) + (y+yd)*(rowsize+2) + x +xd ) ) ) {
-                                occ_count++;
-                            }
-                        }
-                    }
-                    if ( occ_count >= 4 ) {
-                         *((*grd) + y*(rowsize+2) + x ) = 'L';
-                        has_changed = 1;
-                        if ( x > thismaxx ) {
-                           thismaxx = x;
-                        }
-                        if ( y > thismaxy ) {
-                           thismaxy = y;
-                        }
-                        if ( x < thisminx ) {
-                           thisminx = x;
-                        }
-                        if ( y < thisminy ) {
-                           thisminy = y;
-                        }
-                    } else {
-                        *(fixed_seats + + y*(rowsize+2) + x) = 1;
-                    }
-                }
-            }
-        }
-        minx = thisminx;
-        maxx = thismaxx;
-        miny = thisminy;
-        maxy = thismaxy;
-    } while (has_changed);
-
-    int occ_cnt = 0;
-    for ( int y = 1; y <= rows; y++ ) {
-        for ( int x = 1; x <= rowsize; x++ ) {
-            if ( *(*grd + y*(rowsize+2) + x) == '#' ) {
-                occ_cnt++;
-            }
-        }
-    }
-    printf("occupied seats: %d\n", occ_cnt);
-    printtime();
-    //strncpy(grid, grid_p2, (rows+2) * (rowsize+2));
-    grd = &grid_p2; 
-
-    minx = 1;
-    miny = 1;
-    maxx = rowsize;
-    maxy = rows;
-    memset(fixed_seats, 0, (rows+2) * (rowsize+2));
-
-    //for each seat precalculate what the "visible seats" are, so that later we can simply loop over them
-
     int *spos = calloc( (rows+2) * (rowsize+2) * 8, sizeof(int));
     for ( int y = miny; y <= maxy; y++ ) {
         for ( int x = minx; x <= maxx; x++ ) {
             int pos = y*(rowsize+2) + x;
             int seatpos = 0;
-            if ( *((*grd) + pos ) == 'L' ) {
+            if ( *((*grdp1) + pos ) == 'L' ) {
                 for ( int yd = -1; yd < 2; yd++ ) {
                     for ( int xd = -1; xd < 2; xd++ ) {
                         if ( yd == 0 && xd == 0 ) {
                             continue;
                         }
                         for ( int j = 1, xx = x + xd*j, yy = y + yd*j; xx > 0 && xx <= rowsize && yy > 0 && yy <= rows; j++, xx = x + xd*j, yy = y + yd*j ) {
-                            if ( FREE(*((*grd) + (yy)*(rowsize+2) + xx ) ) ) {
+                            if ( FREE(*((*grdp1) + (yy)*(rowsize+2) + xx ) ) ) {
                                 *(spos + (pos << 3) + seatpos ) = (yy)*(rowsize+2) + xx;
                                 seatpos++;
                                 break;
@@ -201,62 +95,61 @@ occ1:
             }
         }
     }
-    printtime();
-    do {
-        char **tmp = grd;
-        grd = pgrid;
-        pgrid = tmp;
 
-        //print_grid(*pgrid, rowsize+2, rows+2);
-        has_changed = 0;
+    do {
+        char **tmp;
+        if ( !p1_done ) {
+            tmp = grdp1;
+            grdp1 = pgridp1;
+            pgridp1 = tmp;
+        }
+
+        if ( !p2_done ) {
+            tmp = grdp2;
+            grdp2 = pgridp2;
+            pgridp2 = tmp;
+        }
+
+//        print_grid(*pgridp2, rowsize+2, rows+2);
+//        iter++;
+//        if (iter > 6 ) {
+//           return 1;
+//        }
 
         int thisminx = maxx;
         int thismaxx = minx;
         int thisminy = maxy;
         int thismaxy = miny;
 
+        if ( !p1_done ) {
+            has_changed_p1 = 0;
+        }
+        if ( !p2_done ) {
+            has_changed_p2 = 0;
+        }
         for ( int y = miny; y <= maxy; y++ ) {
             for ( int x = minx; x <= maxx; x++ ) {
                 int pos = y*(rowsize+2) + x;
-                if ( *(fixed_seats + pos) ) {
-                    continue;
-                }
-                if ( *((*pgrid) + pos ) == 'L' ) {
-                    for ( int i = 0; i < 8 && *(spos + (pos << 3) + i); i++ ) {
-                        if ( OCCUPIED(*(*pgrid + *(spos + (pos << 3) + i))) ) {
-                            goto occ2;
-                        }
+                if ( !p1_done ) {
+                    if ( *(fixed_seats + pos ) ) {
+                        goto p2;
                     }
+                    if ( *(*pgridp1 + pos) == 'L' ) {
+                        *(*grdp1 + pos ) = *(*pgridp1 + pos);
+                        
+                        for ( int yd = -1; yd < 2; yd++ ) {
+                            for ( int xd = -1; xd < 2; xd++ ) {
+                                if ( yd == 0 && xd == 0 ) {
+                                    continue;
+                                }
+                                if ( OCCUPIED( *((*pgridp1) + (y+yd)*(rowsize+2) + x +xd ) ) ) {
+                                    goto occ1;
+                                }
+                            }
+                        }
 
-                    *((*grd) + pos ) = '#';
-                    has_changed = 1;
-                    if ( x > thismaxx ) {
-                       thismaxx = x;
-                    }
-                    if ( y > thismaxy ) {
-                       thismaxy = y;
-                    }
-                    if ( x < thisminx ) {
-                       thisminx = x;
-                    }
-                    if ( y < thisminy ) {
-                       thisminy = y;
-                    } 
-                    continue;
-occ2:               *((*grd) + pos ) = 'L';
-                    *(fixed_seats+ pos) = 1;
-                    
-                } else if ( *((*pgrid) + pos ) == '#' ) {
-                    *((*grd) + y*(rowsize+2) + x ) = *((*pgrid) + y*(rowsize+2) + x );
-                    int occ_count = 0;
-                    for ( int i = 0; i < 8 && *(spos + (pos << 3) + i); i++ ) {
-                        if ( OCCUPIED(*(*pgrid + *(spos + (pos << 3) + i))) ) {
-                            occ_count++;
-                        }
-                    }
-                    if ( occ_count >= 5 ) {
-                        *((*grd) + pos ) = 'L';
-                        has_changed = 1;
+                        *(*grdp1 + pos) = '#';
+                        has_changed_p1 = 1;
                         if ( x > thismaxx ) {
                            thismaxx = x;
                         }
@@ -269,9 +162,97 @@ occ2:               *((*grd) + pos ) = 'L';
                         if ( y < thisminy ) {
                            thisminy = y;
                         }
-                    } else {
-                        *((*grd) + pos ) = '#';
-                        *(fixed_seats+ pos) = 1;
+                        goto p2;
+occ1:
+                        *(fixed_seats + pos ) = 1;
+                    } else if ( *(*pgridp1 + pos) == '#' ) {
+                        *(*grdp1 + pos ) = *(*pgridp1 + pos);
+                        int occ_count = 0;
+                        for ( int yd = -1; yd < 2; yd++ ) {
+                            for ( int xd = -1; xd < 2; xd++ ) {
+                                if ( yd == 0 && xd == 0 ) {
+                                    continue;
+                                }
+                                if ( OCCUPIED( *((*pgridp1) + (y+yd)*(rowsize+2) + x +xd ) ) ) {
+                                    occ_count++;
+                                }
+                            }
+                        }
+                        if ( occ_count >= 4 ) {
+                            *(*grdp1 + pos) = 'L';
+                            has_changed_p1 = 1;
+                            if ( x > thismaxx ) {
+                               thismaxx = x;
+                            }
+                            if ( y > thismaxy ) {
+                               thismaxy = y;
+                            }
+                            if ( x < thisminx ) {
+                               thisminx = x;
+                            }
+                            if ( y < thisminy ) {
+                               thisminy = y;
+                            }
+                        } else {
+                            *(fixed_seats +  pos) = 1;
+                        }
+                    }
+                }
+p2:
+                if ( !p2_done ) {
+                    if ( *(fixed_seats_p2 + pos) ) {
+                        continue;
+                    }
+                    if ( *((*pgridp2) + pos ) == 'L' ) {
+                        for ( int i = 0; i < 8 && *(spos + (pos << 3) + i); i++ ) {
+                            if ( OCCUPIED(*(*pgridp2 + *(spos + (pos << 3) + i))) ) {
+                                goto occ2;
+                            }
+                        }
+                        *((*grdp2) + pos ) = '#';
+                        has_changed_p2 = 1;
+                        if ( x > thismaxx ) {
+                           thismaxx = x;
+                        }
+                        if ( y > thismaxy ) {
+                           thismaxy = y;
+                        }
+                        if ( x < thisminx ) {
+                           thisminx = x;
+                        }
+                        if ( y < thisminy ) {
+                           thisminy = y;
+                        }
+                        continue;
+occ2:                   *((*grdp2) + pos ) = 'L';
+                        *(fixed_seats_p2 + pos) = 1;
+
+                    } else if ( *((*pgridp2) + pos ) == '#' ) {
+                        int occ_count = 0;
+                        for ( int i = 0; i < 8 && *(spos + (pos << 3) + i); i++ ) {
+                            if ( OCCUPIED(*(*pgridp2 + *(spos + (pos << 3) + i))) ) {
+                                occ_count++;
+                            }
+                        }
+                        if ( occ_count >= 5 ) {
+                            *((*grdp2) + pos ) = 'L';
+                            has_changed_p2 = 1;
+                            if ( x > thismaxx ) {
+                               thismaxx = x;
+                            }
+                            if ( y > thismaxy ) {
+                               thismaxy = y;
+                            }
+                            if ( x < thisminx ) {
+                               thisminx = x;
+                            }
+                            if ( y < thisminy ) {
+                               thisminy = y;
+                            }
+                        } else {
+                            *((*grdp2) + pos ) = '#';
+                            *(fixed_seats_p2+ pos) = 1;
+                        }
                     }
                 }
             }
@@ -280,17 +261,26 @@ occ2:               *((*grd) + pos ) = 'L';
         maxx = thismaxx;
         miny = thisminy;
         maxy = thismaxy;
-    } while (has_changed);
+        p1_done = p1_done || !has_changed_p1;
+        p2_done = p2_done || !has_changed_p2;
+    } while (!p1_done || !p2_done);
 
-    occ_cnt = 0;
+    int occ_cnt_p1 = 0;
+    int occ_cnt_p2 = 0;
     for ( int y = 1; y <= rows; y++ ) {
         for ( int x = 1; x <= rowsize; x++ ) {
-            if ( *(*grd + y*(rowsize+2) + x) == '#' ) {
-                occ_cnt++;
+            if ( *(*grdp1 + y*(rowsize+2) + x) == '#' ) {
+                occ_cnt_p1++;
+            }
+            if ( *(*grdp2 + y*(rowsize+2) + x) == '#' ) {
+                occ_cnt_p2++;
             }
         }
     }
-    printf("occupied seats: %d\n", occ_cnt);
+    printf("occupied seats p1: %d\n", occ_cnt_p1);
+    printf("occupied seats p2: %d\n", occ_cnt_p2);
+
     printtime();
+
 }
 
