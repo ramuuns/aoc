895168f9a6be7e8ebd4431e5034cb7c88fc881e1
Ramūns <ramuuns@enkurs.org>
2020-12-13 19:28:34 +0100

    Clean up useless things from day 13 and slightly optimize it
    We only need to check the buses for which we haven't found the modulus,
    so instead of keeping track of that in a variable in the bus list and
    checking that variable every time, we instead replace the current item
    in the array with the last item in the array and decrease the array
    size, thus effectively forgetting about found busses, since we no longer
    care about them.
    
    This aught to cut down on the instructions somewhat.

--- 2020/day-13.c
+++ 2020/day-13.c
@@ -16,16 +16,12 @@ int main() {
     int n = 0;
     while ( (c = fgetc(fp)) ) {
         if ( c == '\n' ) {
-            //if ( n ) {
-                busses[bus_count++] = n;
-            //}
+            busses[bus_count++] = n;
             break;
         }
         if ( c == ',' ) {
-            //if ( n ) {
-                busses[bus_count++] = n;
-                n = 0;
-            //}
+            busses[bus_count++] = n;
+            n = 0;
             continue;
         }
         if ( c == 'x' ) {
@@ -38,41 +34,36 @@ int main() {
     int min_minutes = time;
     int min_bus;
     int nonzero_bus_count = 0;
-    int nz_busses[10][3];
-    int maxbus = 0;
-    int maxbus_idx = 0;
+    int nz_busses[10][2];
     for ( int i = 0; i < bus_count; i++ ) {
         if ( !busses[i] ) {
             continue;
         }
         nz_busses[nonzero_bus_count][0] = i;
-        nz_busses[nonzero_bus_count][2] = 0;
         nz_busses[nonzero_bus_count++][1] = busses[i];
         int delta = busses[i] - (time % busses[i]);
         if ( delta < min_minutes ) {
             min_minutes = delta;
             min_bus = busses[i];
         }
-        if ( busses[i] > maxbus ) {
-            maxbus = busses[i];
-            maxbus_idx = i;
-        }
     }
     printf("bus nr %d, need to wait %d minutes, and magic nr is %d\n", min_bus, min_minutes, min_bus * min_minutes);
-    //unsigned long k = 1;
     unsigned long b1_time = 0;
     unsigned long mul = busses[0];
-    nz_busses[0][2] = 1;
     int num_found = 1;
     while ( 1 ) {
         b1_time += mul;
         int all_good = 1;
-        for ( int i = 0; i < nonzero_bus_count; i++ ) {
+        for ( int i = 1; i < nonzero_bus_count; i++ ) { //we start from 1 because we already know that any number % bus[0] will be 0, so no need to re-confirm that 
             if ( (b1_time + nz_busses[i][0]) % nz_busses[i][1] ) {
                 all_good = 0;
-            } else if ( !nz_busses[i][2] ) {
+            } else {
                 mul *= nz_busses[i][1];
-                nz_busses[i][2] = 1;
+                // put the last bus here in place of this one, and decrement the number of busses that we need to check
+                nz_busses[i][0] = nz_busses[nonzero_bus_count - 1][0];
+                nz_busses[i][1] = nz_busses[nonzero_bus_count - 1][1];
+                i--; // need to check if this also happens to be a mutliple of the previous "last bus", so check again
+                nonzero_bus_count--;
             }
         }
         if ( all_good ) {
