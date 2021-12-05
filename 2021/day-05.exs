cbba0bee8dc579d3cf7731b69eb7484e44b22353
Ramūns Usovs <ramuuns@enkurs.org>
2021-12-05 19:19:07 +0100

    speed up day 5 by 2x by NOT using maps
    So here's the deal with maps in elixir: because they are immutable,
    adding a new key means you create a new map (which is an O(sizeof old_map))
    with with the extra key in it. So basically if you do a lot of
    Map.put(), you can expect the perf to be "not great". Contrast this with
    lists, where because of immutability adding an element at the head is an
    O(1) op, since all we do is create a new struct with a {value : val,
    next: *old_list} and move on with our lives.
    
    This in turn means that it's actually _much_ faster (about 2x) to simply
    create a list that holds all of the points that any of the lines touch,
    and then sort that list and then count the cases where the list has
    items in the form of [x,x,y | rest].
    
    Def add this to "list of things Ramūns has learned about elixir during
    AOC 2021"

--- 2021/day-05.exs
+++ 2021/day-05.exs
@@ -1,5 +1,4 @@
 defmodule Day5 do
-
   def run(mode) do
     start = :erlang.system_time(:microsecond)
 
@@ -53,57 +52,45 @@ defmodule Day5 do
     |> List.to_tuple()
   end
 
-  def part1(lines), do: count_lines({%{}, %{}, 0}, lines, :part1)
+  def part1(lines), do: count_lines([], lines, :part1)
 
-  def part2(lines), do: count_lines({%{}, %{}, 0}, lines, :all)
+  def part2(lines), do: count_lines([], lines, :all)
 
-  def count_lines({_, _, count}, [], _), do: count
+  def do_count([], count), do: count
+  def do_count([x, x, x | rest], count), do: do_count([x, x | rest], count)
+  def do_count([x, x, y | rest], count), do: do_count([y | rest], count + 1)
+  def do_count([_ | rest], count), do: do_count(rest, count)
 
-  def count_lines(grid_acc, [{{x, y1}, {x, y2}} | rest], mode),
+  def count_lines(points, [], _), do: points |> Enum.sort() |> do_count(0)
+
+  def count_lines(points, [{{x, y1}, {x, y2}} | rest], mode),
     do:
-      grid_acc
-      |> add_to_grid(make_line(x, y1..y2 |> Enum.to_list(), :x, []))
+      points
+      |> add_line(x, y1..y2 |> Enum.to_list(), :x)
       |> count_lines(rest, mode)
 
-  def count_lines(grid_acc, [{{x1, y}, {x2, y}} | rest], mode),
+  def count_lines(points, [{{x1, y}, {x2, y}} | rest], mode),
     do:
-      grid_acc
-      |> add_to_grid(make_line(y, x1..x2 |> Enum.to_list(), :y, []))
+      points
+      |> add_line(y, x1..x2 |> Enum.to_list(), :y)
       |> count_lines(rest, mode)
 
-  def count_lines(grid_acc, [_ | rest], :part1), do: count_lines(grid_acc, rest, :part1)
+  def count_lines(points, [_ | rest], :part1), do: count_lines(points, rest, :part1)
 
-  def count_lines(grid_acc, [{{x1, y1}, {x2, y2}} | rest], :all),
+  def count_lines(points, [{{x1, y1}, {x2, y2}} | rest], :all),
     do:
-      grid_acc
-      |> add_to_grid(make_line(x1..x2 |> Enum.to_list(), y1..y2 |> Enum.to_list(), :diag, []))
+      points
+      |> add_line(x1..x2 |> Enum.to_list(), y1..y2 |> Enum.to_list(), :diag)
       |> count_lines(rest, :all)
 
-  #  have the points be represented as a single integer, which will make the map insert/check faster (than if it's a tuple)
+  #  have the points be represented as a single integer
 
-  def make_line(_, [], _, acc), do: acc
-  def make_line(x, [y | rest], :x, acc), do: make_line(x, rest, :x, [x * 1000 + y | acc])
-  def make_line(y, [x | rest], :y, acc), do: make_line(y, rest, :y, [x * 1000 + y | acc])
+  def add_line(points, _, [], _), do: points
+  def add_line(points, x, [y | rest], :x), do: add_line([x * 1000 + y | points], x, rest, :x)
+  def add_line(points, y, [x | rest], :y), do: add_line([x * 1000 + y | points], y, rest, :y)
 
-  def make_line([x | xrest], [y | yrest], :diag, acc),
-    do: make_line(xrest, yrest, :diag, [x * 1000 + y | acc])
-
-  #  the two map solution is slower than if there's an if there's just one map and we check the value in
-  # and increment the counter if the value == 1, but looks a bit more elegant as it lets us outsource the
-  # checking if we should add to the counter in a guard and makes the functions themselves shorter
-
-  def add_to_grid(acc, []), do: acc
-
-  def add_to_grid({_, at_least_two, _} = acc, [point | rest])
-      when is_map_key(at_least_two, point),
-      do: add_to_grid(acc, rest)
-
-  def add_to_grid({at_least_one, at_least_two, cnt}, [point | rest])
-      when is_map_key(at_least_one, point),
-      do: add_to_grid({at_least_one, at_least_two |> Map.put(point, 1), cnt + 1}, rest)
-
-  def add_to_grid({at_least_one, at_least_two, cnt}, [point | rest]),
-    do: add_to_grid({at_least_one |> Map.put(point, 1), at_least_two, cnt}, rest)
+  def add_line(points, [x | xrest], [y | yrest], :diag),
+    do: add_line([x * 1000 + y | points], xrest, yrest, :diag)
 end
 
 Day5.run(:test)
