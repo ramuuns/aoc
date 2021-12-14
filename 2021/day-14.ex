6e0e5842cc5665859a44a86354f5689bd5075215
Ramūns Usovs <ramuuns@enkurs.org>
2021-12-14 14:09:29 +0100

    The non-stupid day 14 version
    Yes, you should count the _pairs_ that way it doesn't matter what
    position they are in ( this is because each pair determines what happens
    in the subsequent levels independently of all other pairs), so you can
    just... count them, and have the transform `AB -> C` be `AB -> [AC, CB]`
    
    The hard part then was figuring out how do I go from a map of pair
    counts to a map of char counts, but that part I cracked after some
    thinking (spoilers count first and second individually, and then just
    get max of each value for each key)

--- 2021/day-14.ex
+++ 2021/day-14.ex
@@ -37,75 +37,90 @@ CN -> C"
     |> prepare_data
   end
 
-  def prepare_data([chain, "" | data]), do: prepare_data(data, chain |> String.split("", trim: true), %{})
+  def prepare_data([chain, "" | data]),
+    do: prepare_data(data, chain |> String.split("", trim: true) |> make_chain(%{}), %{})
+
   def prepare_data([], chain, transforms), do: {chain, transforms}
-  def prepare_data([transform | rest ], chain, transforms), do: prepare_data(rest, chain, transforms |> add_transform(transform |> String.split(" -> ")))
 
-  def add_transform(transforms, [from, to]), do: transforms |> Map.put( from |> String.split("", trim: true) |> List.to_tuple, to)
+  def prepare_data([transform | rest], chain, transforms),
+    do: prepare_data(rest, chain, transforms |> add_transform(transform |> String.split(" -> ")))
 
+  def add_transform(transforms, [from, to]) do
+    [a, b] = from |> String.split("", trim: true)
+    transforms |> Map.put(from, ["#{a}#{to}", "#{to}#{b}"])
+  end
+
+  def make_chain([_ | []], chain), do: chain
+
+  def make_chain([a, b | rest], chain),
+    do: make_chain([b | rest], chain |> Map.put("#{a}#{b}", Map.get(chain, "#{a}#{b}", 0) + 1))
 
   def part1({chain, transforms}) do
-    chain |> grow([], transforms, 10, :normal) |> min_max_diff
+    chain |> Map.to_list() |> grow(%{}, transforms, 10) |> min_max_diff
   end
 
   def part2({chain, transforms}) do
-    {min, max} = chain 
-    |> grow([], transforms, 20, :normal)
-    |> count_pair_growth(transforms, %{}, %{})
-    |> Enum.reduce({nil, nil}, fn                
-      {_, v}, {nil, nil} -> {v, v}
-      {_, v}, {min, max} when v < min -> {v, max}
-      {_, v}, {min, max} when v > max -> {min, v}
-      _, acc -> acc
-    end)
-    max - min
+    chain |> Map.to_list() |> grow(%{}, transforms, 40) |> min_max_diff
   end
 
-  def grow(chain, _, _, 0, _), do: chain
-  def grow([a | []], chain, transforms, n, :normal) do 
-    grow([ a | chain], [], transforms, n - 1, :reversed)
-  end
+  def grow([], chain, _, 1), do: chain
+  def grow([], chain, transforms, n), do: chain |> Map.to_list() |> grow(%{}, transforms, n - 1)
 
-  def grow([a | []], chain, transforms, n, :reversed) do
-    grow([a | chain], [], transforms, n - 1, :normal)
+  def grow([{pair, cnt} | rest], chain, transforms, n) do
+    [p1, p2] = transforms |> Map.get(pair)
+
+    grow(
+      rest,
+      chain
+      |> Map.put(p1, Map.get(chain, p1, 0) + cnt)
+      |> Map.put(p2, Map.get(chain, p2, 0) + cnt),
+      transforms,
+      n
+    )
   end
-  def grow([a, b | chain], newchain, transforms, n, :normal) when not is_map_key(transforms, {a,b}), 
-    do: grow([b | chain], [ a | newchain ], transforms, n, :normal)
-  def grow([a, b | chain], newchain, transforms, n, :reversed) when not is_map_key(transforms, {b,a}), 
-    do: grow([b | chain], [ a | newchain ], transforms, n, :reversed)
-  def grow([a, b | chain], newchain, transforms, n, :normal), do: grow([b | chain], [transforms |> Map.get({a,b}), a | newchain], transforms, n, :normal)
-  def grow([a, b | chain], newchain, transforms, n, :reversed), do: grow([b | chain], [transforms |> Map.get({b,a}), a | newchain], transforms, n, :reversed)
 
   def min_max_diff(chain) do
-    {min, max} = chain |> Enum.frequencies() |> Enum.reduce({nil, nil}, fn 
-      {_, v}, {nil, nil} -> {v, v}
-      {_, v}, {min, max} when v < min -> {v, max}
-      {_, v}, {min, max} when v > max -> {min, v}
-      _, acc -> acc
-    end) 
+    {min, max} =
+      chain
+      |> Enum.reduce({%{}, %{}}, fn
+        {pair, cnt}, {aacc, bacc} ->
+          [a, b] = pair |> String.split("", trim: true)
+
+          {
+            aacc |> Map.put(a, Map.get(aacc, a, 0) + cnt),
+            bacc |> Map.put(b, Map.get(bacc, b, 0) + cnt)
+          }
+      end)
+      |> then(fn
+        {apos, bpos} ->
+          apos
+          |> Map.keys()
+          |> MapSet.new()
+          |> MapSet.union(
+            bpos
+            |> Map.keys()
+            |> MapSet.new()
+          )
+          |> Enum.reduce(%{}, fn
+            key, acc ->
+              acc
+              |> Map.put(
+                key,
+                [
+                  apos |> Map.get(key, 0),
+                  bpos |> Map.get(key, 0)
+                ]
+                |> Enum.max()
+              )
+          end)
+      end)
+      |> Enum.reduce({nil, nil}, fn
+        {_, v}, {nil, nil} -> {v, v}
+        {_, v}, {min, max} when v < min -> {v, max}
+        {_, v}, {min, max} when v > max -> {min, v}
+        _, acc -> acc
+      end)
+
     max - min
-  end
-
-  def count_pair_growth([b | []], _, _, chars), do: chars |> Map.put(b, Map.get(chars, b) + 1)
-  def count_pair_growth([a,b | rest], transforms, pair_counts, chars) when is_map_key(pair_counts, {a,b}) do
-    count_pair_growth([b | rest], transforms, pair_counts, chars |> add_count(pair_counts |> Map.get({a,b}), a,b))
-  end
-
-  def count_pair_growth([a, b | rest], transforms, pair_counts, chars) do
-    pair_counts_this_pair = [a,b] |> grow([], transforms, 20, :normal) |> Enum.frequencies() |> Map.map(fn
-      {^a, v} -> v - 1
-      {^b, v} -> v - 1
-      {_, v} -> v 
-    end)
-    count_pair_growth([b | rest], transforms, pair_counts |> Map.put({a,b}, pair_counts_this_pair), chars |> add_count(pair_counts_this_pair, a,b))
-  end
-
-
-  def add_count(to, what, a, b) do
-    chars = what |> Enum.reduce(to, fn {k, v}, acc -> acc |> Map.put(k, Map.get(acc, k, 0) + v) end)
-    chars 
-    |> Map.put(a, Map.get(chars, a) + 1)
-    # this is in case a == b we need to _not_ add anything to it, so if that's the case then we simply overwrite it with the count as was
-    |> Map.put(b, Map.get(chars, b) + 0)
   end
 end
