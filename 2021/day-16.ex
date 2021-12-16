e9bfb12f205ca23ef6c87060532b240a7b281006
Ramūns Usovs <ramuuns@enkurs.org>
2021-12-17 00:07:23 +0100

    And now with only tail recursion + our own stack
    not sure that this code ends up being nicer (what with having to keep
    dragging the stack around and whatnot), but like it does let us get rid
    the whole parse_n_subpackets() thing

--- 2021/day-16.ex
+++ 2021/day-16.ex
@@ -29,93 +29,91 @@ defmodule Day16 do
   end
 
   def part1(data) do
-    recurse_decode_packets(data, :version_num, 0)
+    recurse_decode_packets(data, :version_num, 0, nil)
   end
 
   def part2(data) do
-    recurse_decode_packets(data, :eval, []) |> Enum.at(0)
+    recurse_decode_packets(data, :eval, [], [{-1, -1, nil, nil}]) |> Enum.at(0)
   end
 
-  def recurse_decode_packets(<<version::3, 4::3, rest::bits>>, :version_num, acc) do
-    {rest, _} = read_value(rest, 0)
-    recurse_decode_packets(rest, :version_num, acc + version)
+  def recurse_decode_packets(<<version::3, 4::3, rest::bits>>, :version_num, acc, nil) do
+    {rest, _, _} = read_value(rest, 0, 0)
+    recurse_decode_packets(rest, :version_num, acc + version, nil)
   end
 
   def recurse_decode_packets(
         <<version::3, _::3, 1::1, _::11, rest::bits>>,
         :version_num,
-        acc
+        acc,
+        nil
       ),
-      do: recurse_decode_packets(rest, :version_num, acc + version)
+      do: recurse_decode_packets(rest, :version_num, acc + version, nil)
 
   def recurse_decode_packets(
         <<version::3, _::3, 0::1, _::15, rest::bits>>,
         :version_num,
-        acc
+        acc,
+        nil
       ),
-      do: recurse_decode_packets(rest, :version_num, acc + version)
+      do: recurse_decode_packets(rest, :version_num, acc + version, nil)
 
-  def recurse_decode_packets(<<_::3, 4::3, rest::bits>>, :eval, acc) do
-    {rest, val} = read_value(rest, 0)
-    recurse_decode_packets(rest, :eval, [val | acc])
+  # pop the stack on "n packets have been read"
+  # note that we have to also ensure that we "add" the number of bits we've read while doing so to the next level of the stack
+  def recurse_decode_packets(bits, :eval, acc, [
+        {0, bits_read, op, stacked_acc},
+        {n, b, op2, s2} | opstack
+      ]),
+      do:
+        recurse_decode_packets(bits, :eval, [acc |> calc_value(op) | stacked_acc], [
+          {n, b + (1 + bits_read), op2, s2} | opstack
+        ])
+
+  # pop the stack on "we've read n bits"
+  def recurse_decode_packets(bits, :eval, acc, [{_, 0, op, stacked_acc} | opstack]),
+    do: recurse_decode_packets(bits, :eval, [acc |> calc_value(op) | stacked_acc], opstack)
+
+  #  read the value packets
+  def recurse_decode_packets(<<_::3, 4::3, rest::bits>>, :eval, acc, [{n, b, op, stacc} | opstack]) do
+    {rest, val, bits_read} = read_value(rest, 0, 0)
+
+    recurse_decode_packets(rest, :eval, [val | acc], [
+      {n - 1, b - bits_read - 6, op, stacc} | opstack
+    ])
   end
 
+  # read the subpacket has sub_len bits op packet
   def recurse_decode_packets(
         <<_::3, type::3, 0::1, sub_len::15, rest::bits>>,
         :eval,
-        acc
+        acc,
+        [{n, b, op, stacc} | opstack]
       ) do
-    <<sub_rest::bits-size(sub_len), rest::bits>> = rest
-    values = recurse_decode_packets(sub_rest, :eval, [])
-    value = calc_value(values, type)
-    recurse_decode_packets(rest, :eval, [value | acc])
+    recurse_decode_packets(rest, :eval, [], [
+      {-1, sub_len, type, acc},
+      {n - 1, b - sub_len - 22, op, stacc} | opstack
+    ])
   end
 
+  # read the subpacket has num_subpackets operands op packet
   def recurse_decode_packets(
         <<_::3, type::3, 1::1, num_subpackets::11, rest::bits>>,
         :eval,
-        acc
-      ) do
-    {values, rest} = decode_n_subpackets(rest, [], num_subpackets)
-    value = calc_value(values, type)
-    recurse_decode_packets(rest, :eval, [value | acc])
-  end
-
-  def recurse_decode_packets(_, _, acc), do: acc
-
-  def decode_n_subpackets(rest, acc, 0), do: {acc, rest}
-
-  def decode_n_subpackets(<<_::3, 4::3, rest::bits>>, acc, n) do
-    {rest, val} = read_value(rest, 0)
-    decode_n_subpackets(rest, [val | acc], n - 1)
-  end
-
-  def decode_n_subpackets(
-        <<_::3, type::3, 0::1, sub_len::15, rest::bits>>,
         acc,
-        n
+        [{n, b, op, stacc} | opstack]
       ) do
-    <<sub_rest::bits-size(sub_len), rest::bits>> = rest
-    values = recurse_decode_packets(sub_rest, :eval, [])
-    value = calc_value(values, type)
-    decode_n_subpackets(rest, [value | acc], n - 1)
+    recurse_decode_packets(rest, :eval, [], [
+      {num_subpackets, -1, type, acc},
+      {n - 1, b - 18, op, stacc} | opstack
+    ])
   end
 
-  def decode_n_subpackets(
-        <<_::3, type::3, 1::1, num_subpackets::11, rest::bits>>,
-        acc,
-        n
-      ) do
-    {values, rest} = decode_n_subpackets(rest, [], num_subpackets)
-    value = calc_value(values, type)
-    decode_n_subpackets(rest, [value | acc], n - 1)
-  end
+  def recurse_decode_packets(_, _, acc, _), do: acc
 
-  def read_value(<<1::1, n::4, rest::bits>>, val),
-    do: read_value(rest, (val <<< 4) + n)
+  def read_value(<<1::1, n::4, rest::bits>>, val, bits),
+    do: read_value(rest, (val <<< 4) + n, bits + 5)
 
-  def read_value(<<0::1, n::4, rest::bits>>, val),
-    do: {rest, (val <<< 4) + n}
+  def read_value(<<0::1, n::4, rest::bits>>, val, bits),
+    do: {rest, (val <<< 4) + n, bits + 5}
 
   def calc_value(values, 0), do: Enum.sum(values)
   def calc_value(values, 1), do: Enum.reduce(values, 1, fn n, p -> p * n end)
