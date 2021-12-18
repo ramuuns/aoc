3419958dee43037aded833dc46c4fde183443add
Ramūns Usovs <ramuuns@enkurs.org>
2021-12-18 18:30:21 +0100

    make day 18 _slightly_ faster
    because several hours of debugging recursion in the morning wasn't
    enough, I decided that I clearly need to debug it some more, by not only
    keeping track track of depth at each node, but also if it requires a
    split (which makes splitting _slightly_ faster) so that we always know
    where and how to do splits.
    
    Anyhow now the data structure is {depth, needs_split?, [l, r]}, and I
    guess the splitting functions are simpler now (because we don't need to
    keep track if we split something or not). That said, we pay for all
    this, by having to ensure that we know if any node needs a split and
    propogating that up the tree.

--- 2021/day-18.ex
+++ 2021/day-18.ex
@@ -38,22 +38,22 @@ defmodule Day18 do
     end)
   end
 
-  def to_structured([a, b]) when is_integer(a) and is_integer(b), do: {0, [a, b]}
+  def to_structured([a, b]) when is_integer(a) and is_integer(b), do: {0, false, [a, b]}
 
   def to_structured([a, b]) when is_integer(a) and is_list(b) do
     b = b |> to_structured
-    {elem(b, 0) + 1, [a, b]}
+    {elem(b, 0) + 1, false, [a, b]}
   end
 
   def to_structured([a, b]) when is_integer(b) and is_list(a) do
     a = a |> to_structured
-    {elem(a, 0) + 1, [a, b]}
+    {elem(a, 0) + 1, false, [a, b]}
   end
 
   def to_structured([a, b]) do
     a = a |> to_structured
     b = b |> to_structured
-    {Enum.max([elem(a, 0), elem(b, 0)]) + 1, [a, b]}
+    {Enum.max([elem(a, 0), elem(b, 0)]) + 1, false, [a, b]}
   end
 
   def part1([first | data]) do
@@ -68,142 +68,133 @@ defmodule Day18 do
     find_max_sum(data, data, 0)
   end
 
-  def add({d1, a}, {d2, b}), do: {Enum.max([d1, d2]) + 1, [{d1, a}, {d2, b}]} |> maybe_reduce
+  def add({d1, _, a}, {d2, _, b}),
+    do: {Enum.max([d1, d2]) + 1, false, [{d1, false, a}, {d2, false, b}]} |> maybe_reduce
 
-  def maybe_reduce({d, _} = n) when d < 4, do: n |> maybe_split
+  def maybe_reduce({d, _, _} = n) when d < 4, do: n |> maybe_split
 
   def maybe_reduce(n) do
     n |> explode |> maybe_reduce
   end
 
-  def explode({_, [{da, a}, {db, b}]}) when da >= db do
-    {{da, a}, _, right} = explode_inner({da, a})
-    {db, b} = add_left({db, b}, right)
-    {Enum.max([da, db]) + 1, [{da, a}, {db, b}]}
+  def explode({_, s, [{da, sa, a}, {db, sb, b}]}) when da >= db do
+    {{da, sa, a}, _, right} = explode_inner({da, sa, a})
+    {db, sb, b} = add_left({db, sb, b}, right)
+    {Enum.max([da, db]) + 1, s or sa or sb, [{da, sa, a}, {db, sb, b}]}
   end
 
-  def explode({_, [{da, a}, {db, b}]}) do
-    {{db, b}, left, _} = explode_inner({db, b})
-    {da, a} = add_right({da, a}, left)
-    {Enum.max([da, db]) + 1, [{da, a}, {db, b}]}
+  def explode({_, s, [{da, sa, a}, {db, sb, b}]}) do
+    {{db, sb, b}, left, _} = explode_inner({db, sb, b})
+    {da, sa, a} = add_right({da, sa, a}, left)
+    {Enum.max([da, db]) + 1, s or sa or sb, [{da, sa, a}, {db, sb, b}]}
   end
 
-  def explode_inner({0, [left, right]}), do: {nil, left, right}
+  def explode_inner({0, _, [left, right]}), do: {nil, left, right}
 
-  def explode_inner({_, [{ld, l}, {rd, r}]}) when ld >= rd do
-    {newl, left, right} = explode_inner({ld, l})
+  def explode_inner({_, _, [{ld, sl, l}, {rd, sr, r}]}) when ld >= rd do
+    {newl, left, right} = explode_inner({ld, sl, l})
 
     case newl do
       nil ->
-        {rd, r} = add_left({rd, r}, right)
-        {{rd + 1, [0, {rd, r}]}, left, 0}
+        {rd, sr, r} = add_left({rd, sr, r}, right)
+        {{rd + 1, sr, [0, {rd, sr, r}]}, left, 0}
 
-      {ld, l} ->
-        {{Enum.max([ld, rd]) + 1, [{ld, l}, add_left({rd, r}, right)]}, left, 0}
+      {ld, sl, l} ->
+        {rd, sr, r} = add_left({rd, sr, r}, right)
+        {{Enum.max([ld, rd]) + 1, sr or sl, [{ld, sl, l}, {rd, sr, r}]}, left, 0}
     end
   end
 
-  def explode_inner({_, [{ld, l}, {rd, r}]}) when ld < rd do
-    {newr, left, right} = explode_inner({rd, r})
+  def explode_inner({_, _, [{ld, sl, l}, {rd, sr, r}]}) when ld < rd do
+    {newr, left, right} = explode_inner({rd, sr, r})
 
     case newr do
       nil ->
-        {ld, l} = add_right({ld, l}, left)
-        {{ld + 1, [{ld, l}, 0], 0, right}}
+        {ld, sl, l} = add_right({ld, sl, l}, left)
+        {{ld + 1, sl, [{ld, sl, l}, 0], 0, right}}
 
-      {rd, r} ->
-        {{Enum.max([ld, rd]) + 1, [add_right({ld, l}, left), {rd, r}]}, 0, right}
+      {rd, sr, r} ->
+        {ld, sl, l} = add_right({ld, sl, l}, left)
+        {{Enum.max([ld, rd]) + 1, sl or sr, [{ld, sl, l}, {rd, sr, r}]}, 0, right}
     end
   end
 
-  def explode_inner({_, [{ld, l}, r]}) do
-    {newl, left, right} = explode_inner({ld, l})
+  def explode_inner({_, _, [{ld, sl, l}, r]}) do
+    {newl, left, right} = explode_inner({ld, sl, l})
 
     case newl do
       nil ->
-        {{0, [0, right + r]}, left, 0}
+        {{0, right + r > 9, [0, right + r]}, left, 0}
 
-      {ld, l} ->
-        {{ld + 1, [{ld, l}, right + r]}, left, 0}
+      {ld, sl, l} ->
+        {{ld + 1, sl or right + r > 9, [{ld, sl, l}, right + r]}, left, 0}
     end
   end
 
-  def explode_inner({_, [l, {rd, r}]}) do
-    {newr, left, right} = explode_inner({rd, r})
+  def explode_inner({_, _, [l, {rd, sr, r}]}) do
+    {newr, left, right} = explode_inner({rd, sr, r})
 
     case newr do
       nil ->
-        {{0, [left + l, 0]}, 0, right}
+        {{0, left + l > 9, [left + l, 0]}, 0, right}
 
-      {rd, r} ->
-        {{rd + 1, [left + l, {rd, r}]}, 0, right}
+      {rd, sr, r} ->
+        {{rd + 1, sr or left + l > 9, [left + l, {rd, sr, r}]}, 0, right}
     end
   end
 
   def add_left(l, 0), do: l
-  def add_left({d, [l, r]}, n) when is_integer(l), do: {d, [l + n, r]}
-  def add_left({d, [l, r]}, n), do: {d, [add_left(l, n), r]}
+  def add_left({d, s, [l, r]}, n) when is_integer(l), do: {d, s or l + n > 9, [l + n, r]}
+
+  def add_left({d, s, [l, r]}, n) do
+    {_, sl, _} = l = add_left(l, n)
+    {d, s or sl, [l, r]}
+  end
 
   def add_right(r, 0), do: r
-  def add_right({d, [l, r]}, n) when is_integer(r), do: {d, [l, r + n]}
-  def add_right({d, [l, r]}, n), do: {d, [l, add_right(r, n)]}
+  def add_right({d, s, [l, r]}, n) when is_integer(r), do: {d, s or r + n > 9, [l, r + n]}
 
-  def maybe_split(num) do
-    {did_split, num} = try_split(num)
-
-    if did_split do
-      num |> maybe_reduce
-    else
-      num
-    end
+  def add_right({d, s, [l, r]}, n) do
+    {_, sr, _} = r = add_right(r, n)
+    {d, s or sr, [l, r]}
   end
 
-  def depth({d, [_, _]}), do: d
+  def maybe_split({_, false, _} = num), do: num
+
+  def maybe_split(num), do: try_split(num) |> maybe_reduce
+
+  def depth({d, _, [_, _]}), do: d
   def depth(_), do: 0
 
-  def try_split({_, [a, b]}) when is_integer(a) and a > 9,
-    do: {true, {Enum.max([0, depth(b)]) + 1, [{0, [div(a, 2), ceil(a / 2)]}, b]}}
+  def needs_split({_, s, _}), do: s
+  def needs_split(n), do: n > 9
 
-  def try_split({_, [a, b]}) when is_tuple(a) and is_tuple(b) do
-    {did_split?, a} = try_split(a)
+  def try_split({_, false, _} = n), do: n
 
-    if did_split? do
-      {true, {Enum.max([depth(a), depth(b)]) + 1, [a, b]}}
-    else
-      {did_split?, b} = try_split(b)
-      {did_split?, {Enum.max([depth(a), depth(b)]) + 1, [a, b]}}
-    end
+  def try_split({_, true, [a, b]}) when is_integer(a) and a > 9,
+    do:
+      {Enum.max([0, depth(b)]) + 1, needs_split(b) or ceil(a / 2) > 9,
+       [{0, ceil(a / 2) > 9, [div(a, 2), ceil(a / 2)]}, b]}
+
+  def try_split({_, true, [{_, true, _} = a, b]}) do
+    a = try_split(a)
+    {Enum.max([depth(a), depth(b)]) + 1, needs_split(a) or needs_split(b), [a, b]}
   end
 
-  def try_split({_, [a, b]}) when is_tuple(a) and is_integer(b) and b < 10 do
-    {did_split?, a} = try_split(a)
-    {did_split?, {Enum.max([depth(a), depth(b)]) + 1, [a, b]}}
+  def try_split({_, true, [a, {_, true, _} = b]}) do
+    b = try_split(b)
+    {Enum.max([depth(a), depth(b)]) + 1, needs_split(a) or needs_split(b), [a, b]}
   end
 
-  def try_split({_, [a, b]}) when is_tuple(a) and is_integer(b) and b > 9 do
-    {did_split?, a} = try_split(a)
+  def try_split({_, true, [a, b]}) when is_integer(b) and b > 9,
+    do:
+      {Enum.max([0, depth(b)]) + 1, needs_split(a) or ceil(b / 2) > 9,
+       [a, {0, ceil(b / 2) > 9, [div(b, 2), ceil(b / 2)]}]}
 
-    if did_split? do
-      {did_split?, {Enum.max([depth(a), depth(b)]) + 1, [a, b]}}
-    else
-      {true, {Enum.max([depth(a), 0]) + 1, [a, {0, [div(b, 2), ceil(b / 2)]}]}}
-    end
-  end
-
-  def try_split({_, [a, b]}) when is_tuple(b) do
-    {did_split?, b} = try_split(b)
-    {did_split?, {Enum.max([depth(a), depth(b)]) + 1, [a, b]}}
-  end
-
-  def try_split({_, [a, b]}) when is_integer(b) and b > 9,
-    do: {true, {Enum.max([0, depth(a)]) + 1, [a, {0, [div(b, 2), ceil(b / 2)]}]}}
-
-  def try_split(n), do: {false, n}
-
-  def magnitude({_, [a, b]}) when is_integer(a) and is_integer(b), do: a * 3 + b * 2
-  def magnitude({_, [a, b]}) when is_integer(a), do: a * 3 + magnitude(b) * 2
-  def magnitude({_, [a, b]}) when is_integer(b), do: magnitude(a) * 3 + b * 2
-  def magnitude({_, [a, b]}), do: magnitude(a) * 3 + magnitude(b) * 2
+  def magnitude({_, _, [a, b]}) when is_integer(a) and is_integer(b), do: a * 3 + b * 2
+  def magnitude({_, _, [a, b]}) when is_integer(a), do: a * 3 + magnitude(b) * 2
+  def magnitude({_, _, [a, b]}) when is_integer(b), do: magnitude(a) * 3 + b * 2
+  def magnitude({_, _, [a, b]}), do: magnitude(a) * 3 + magnitude(b) * 2
 
   def find_max_sum([], _, sum), do: sum
 
