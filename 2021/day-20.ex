70a0ee2bc21aff41475a09191189885bba40bdd2
Ramūns Usovs <ramuuns@enkurs.org>
2021-12-20 15:13:50 +0100

    Improve perf of day 20
    yeah, no so GIANT MAPS ARE SLOW, especially if you keep mutating them,
    and in this case you only need to convert it to a map once you're done
    processing, so we can keep a list during the rest of the time.
    
    Also use integer map keys as those are WAAAAY faster than tuple ones
    and yeah, don't look for an actual minx, maxy, etc, just get the
    starting ones and assume they increase by 1 each time and that's that
    
    so now this runs under a second

--- 2021/day-20.ex
+++ 2021/day-20.ex
@@ -36,54 +36,78 @@ defmodule Day20 do
        {"#", i} -> {i, 1}
        {".", i} -> {i, 0}
      end)
-     |> Enum.into(%{}), img_data |> to_image_map(%{}, 0)}
+     |> Enum.into(%{}), img_data |> to_image_map(%{}, 0, 0)}
   end
 
-  def to_image_map([], ret, _), do: ret
+  def to_image_map([], ret, y, x), do: {y, x, ret}
 
-  def to_image_map([row | rest], ret, y),
-    do: to_image_map(rest, row |> String.split("", trim: true) |> make_row(ret, y, 0), y + 1)
+  def to_image_map([row | rest], ret, y, _) do
+    {x, ret} = row |> String.split("", trim: true) |> make_row(ret, y, 0)
+    to_image_map(rest, ret, y + 1, x)
+  end
 
-  def make_row([], ret, _, _), do: ret
-  def make_row(["#" | rest], ret, y, x), do: make_row(rest, ret |> Map.put({x, y}, 1), y, x + 1)
-  def make_row(["." | rest], ret, y, x), do: make_row(rest, ret |> Map.put({x, y}, 0), y, x + 1)
+  def make_row([], ret, _, x), do: {x, ret}
 
-  def part1({img_ench, image}) do
-    enhance(image, img_ench, 2, img_ench[0], %{0 => img_ench[0], 1 => img_ench[511]})
+  def make_row(["#" | rest], ret, y, x),
+    do: make_row(rest, ret |> Map.put(x * 10000 + y, 1), y, x + 1)
+
+  def make_row(["." | rest], ret, y, x),
+    do: make_row(rest, ret |> Map.put(x * 10000 + y, 0), y, x + 1)
+
+  def part1({img_ench, {y, x, image}}) do
+    enhance(
+      image,
+      img_ench,
+      2,
+      img_ench[0],
+      %{0 => img_ench[0], 1 => img_ench[511]},
+      {0, x, 0, y}
+    )
     |> Map.values()
     |> Enum.filter(fn v -> v == 1 end)
     |> Enum.count()
   end
 
-  def part2({img_ench, image}) do
-    enhance(image, img_ench, 50, img_ench[0], %{0 => img_ench[0], 1 => img_ench[511]})
+  def part2({img_ench, {y, x, image}}) do
+    enhance(
+      image,
+      img_ench,
+      50,
+      img_ench[0],
+      %{0 => img_ench[0], 1 => img_ench[511]},
+      {0, x, 0, y}
+    )
     |> Map.values()
     |> Enum.filter(fn v -> v == 1 end)
     |> Enum.count()
   end
 
-  def enhance(img, _, 0, _, _), do: img
+  def enhance(img, _, 0, _, _, _), do: img
 
-  def enhance(img_map, img_ench, steps, def_idx, defaults) do
-    {minx, maxx, miny, maxy} = img_map |> Map.keys() |> fin_min_max_xy(0, 0, 0, 0)
-
+  def enhance(img_map, img_ench, steps, def_idx, defaults, {minx, maxx, miny, maxy}) do
     (minx - 3)..(maxx + 3)
     |> Enum.to_list()
     |> transform_image(
       (miny - 3)..(maxy + 3) |> Enum.to_list(),
-      %{},
+      [],
       img_map,
       img_ench,
       defaults[def_idx]
     )
-    |> enhance(img_ench, steps - 1, defaults[def_idx], defaults)
+    |> enhance(
+      img_ench,
+      steps - 1,
+      defaults[def_idx],
+      defaults,
+      {minx - 1, maxx + 1, miny - 1, maxy + 1}
+    )
   end
 
-  def print_image(img) do
-    {minx, maxx, miny, maxy} = img |> Map.keys() |> fin_min_max_xy(0, 0, 0, 0)
+  def print_image(img, {minx, maxx, miny, maxy}) do
+    {minx, maxx, miny, maxy} |> IO.inspect()
 
     for y <- miny..maxy do
-      row = for x <- minx..maxx, do: img |> Map.get({x, y}, 0)
+      row = for x <- minx..maxx, do: img |> Map.get(x * 10000 + y, 0)
 
       row
       |> Enum.map(fn
@@ -97,7 +121,7 @@ defmodule Day20 do
     img
   end
 
-  def transform_image([], _, ret, _, _, _), do: ret
+  def transform_image([], _, ret, _, _, _), do: ret |> Map.new()
 
   def transform_image([x | rest], y, ret, src, ench, default),
     do:
@@ -114,61 +138,21 @@ defmodule Day20 do
 
   def transform_line(ret, x, [y | rest], src, ench, default),
     do:
-      ret
-      |> maybe_put({x, y}, get_pixel(x, y, src, ench, default))
+      [{x * 10000 + y, get_pixel(x, y, src, ench, default)} | ret]
       |> transform_line(x, rest, src, ench, default)
-
-  def maybe_put(ret, c, 0), do: ret |> Map.put(c, 0)
-  def maybe_put(ret, c, 1), do: ret |> Map.put(c, 1)
 
   def get_pixel(x, y, src, ench, default) do
     crd =
-      [
-        src |> Map.get({x - 1, y - 1}, default),
-        src |> Map.get({x, y - 1}, default),
-        src |> Map.get({x + 1, y - 1}, default),
-        src |> Map.get({x - 1, y}, default),
-        src |> Map.get({x, y}, default),
-        src |> Map.get({x + 1, y}, default),
-        src |> Map.get({x - 1, y + 1}, default),
-        src |> Map.get({x, y + 1}, default),
-        src |> Map.get({x + 1, y + 1}, default)
-      ]
-      |> to_number(0)
+      256 * Map.get(src, (x - 1) * 10000 + y - 1, default) +
+        128 * Map.get(src, x * 10000 + y - 1, default) +
+        64 * Map.get(src, (x + 1) * 10000 + y - 1, default) +
+        32 * Map.get(src, (x - 1) * 10000 + y, default) +
+        16 * Map.get(src, x * 10000 + y, default) +
+        8 * Map.get(src, (x + 1) * 10000 + y, default) +
+        4 * Map.get(src, (x - 1) * 10000 + y + 1, default) +
+        2 * Map.get(src, x * 10000 + y + 1, default) +
+        Map.get(src, (x + 1) * 10000 + y + 1, default)
 
     ench |> Map.get(crd)
   end
-
-  def to_number([], r), do: r
-  def to_number([1 | rest], r), do: to_number(rest, (r <<< 1) + 1)
-  def to_number([0 | rest], r), do: to_number(rest, r <<< 1)
-
-  def fin_min_max_xy([], minx, maxx, miny, maxy), do: {minx, maxx, miny, maxy}
-
-  def fin_min_max_xy([{x, y} | rest], minx, maxx, miny, maxy) when x < minx and y < miny,
-    do: fin_min_max_xy(rest, x, maxx, y, maxy)
-
-  def fin_min_max_xy([{x, y} | rest], minx, maxx, miny, maxy) when x > maxx and y > maxy,
-    do: fin_min_max_xy(rest, minx, x, miny, y)
-
-  def fin_min_max_xy([{x, y} | rest], minx, maxx, miny, maxy) when x > maxx and y < miny,
-    do: fin_min_max_xy(rest, minx, x, y, maxy)
-
-  def fin_min_max_xy([{x, y} | rest], minx, maxx, miny, maxy) when x < minx and y > maxy,
-    do: fin_min_max_xy(rest, x, maxx, miny, y)
-
-  def fin_min_max_xy([{x, _} | rest], minx, maxx, miny, maxy) when x > maxx,
-    do: fin_min_max_xy(rest, minx, x, miny, maxy)
-
-  def fin_min_max_xy([{x, _} | rest], minx, maxx, miny, maxy) when x < minx,
-    do: fin_min_max_xy(rest, x, maxx, miny, maxy)
-
-  def fin_min_max_xy([{_, y} | rest], minx, maxx, miny, maxy) when y < miny,
-    do: fin_min_max_xy(rest, minx, maxx, y, maxy)
-
-  def fin_min_max_xy([{_, y} | rest], minx, maxx, miny, maxy) when y > maxy,
-    do: fin_min_max_xy(rest, minx, maxx, miny, y)
-
-  def fin_min_max_xy([_ | rest], minx, maxx, miny, maxy),
-    do: fin_min_max_xy(rest, minx, maxx, miny, maxy)
 end
