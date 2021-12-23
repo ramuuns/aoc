7677d89f0fe341417df24fe3d42f1f9a5f0c0bfd
Ramūns Usovs <ramuuns@enkurs.org>
2021-12-23 20:47:24 +0100

    Made my pq a bit faster
    at least for inserts (since actually we usually do way more of those) at
    the cost of slower pop (which can now in the worst case takes O(n
    priorities)).
    
    this does end up being faster in real life, and I guess is pretty ok,
    for a totally naive version.
    
    I do wonder how it stacks up with other versions

--- 2021/priority_queue.ex
+++ 2021/priority_queue.ex
@@ -1,27 +1,35 @@
 defmodule PriorityQueue do
   def new() do
-    {[], %{}}
+    {nil, %{}}
   end
 
-  def add({priorities, values}, prio, value) do
-    {maybe_add_prio(priorities, prio, []),
-     values |> Map.put(prio, [value | values |> Map.get(prio, [])])}
+  def add({min_prio, values}, prio, value) do
+    {if min_prio < prio do
+       min_prio
+     else
+       prio
+     end, values |> Map.put(prio, [value | values |> Map.get(prio, [])])}
   end
 
-  defp maybe_add_prio([], n, acc), do: [n | acc] |> Enum.reverse()
-  defp maybe_add_prio([n | rest], n, acc), do: ([n | acc] |> Enum.reverse()) ++ rest
-  defp maybe_add_prio([a | rest], n, acc) when n < a, do: ([a, n | acc] |> Enum.reverse()) ++ rest
-  defp maybe_add_prio([a | rest], n, acc), do: maybe_add_prio(rest, n, [a | acc])
+  def pop_next({nil, _} = pq), do: {nil, pq}
 
-  def pop_next({[], _} = pq), do: {nil, pq}
-
-  def pop_next({[prio | rest_prios] = all_prio, values}) do
-    [value | rest_values] = values |> Map.get(prio)
+  def pop_next({min_prio, values}) do
+    [value | rest_values] = values |> Map.get(min_prio)
 
     if Enum.empty?(rest_values) do
-      {value, {rest_prios, values |> Map.delete(prio)}}
+      {value, {values |> Map.keys() |> min_key(min_prio), values |> Map.delete(min_prio)}}
     else
-      {value, {all_prio, values |> Map.put(prio, rest_values)}}
+      {value, {min_prio, values |> Map.put(min_prio, rest_values)}}
     end
   end
+
+  defp min_key(keys, old_min) do
+    find_smaller(keys, old_min, nil)
+  end
+
+  defp find_smaller([], _, ret), do: ret
+  defp find_smaller([old | rest], old, min), do: find_smaller(rest, old, min)
+  defp find_smaller([min | rest], old, nil), do: find_smaller(rest, old, min)
+  defp find_smaller([min | rest], old, larger) when min < larger, do: find_smaller(rest, old, min)
+  defp find_smaller([_ | rest], old, min), do: find_smaller(rest, old, min)
 end
