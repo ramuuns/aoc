674b906fece21a5110d3a9c1057c5b37696e3263
Ramūns Usovs <ramuuns@enkurs.org>
2022-12-25 05:50:53 +0200

    Ok this is now reasonable time wise
    did the thing where no longer branch on each t and checking if something
    can be built there, but on next build actions + don't build shit up to the
    end.
    
    now runs in about 30 seconds, which still not great, but like no longer
    awful awfulness

--- 2022/Day19.pm
+++ 2022/Day19.pm
@@ -22,7 +22,6 @@ sub read_input($mode) {
     return prepare_data(<<TEST_DATA) if $mode eq "test";
 Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
-Blueprint 28: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 11 clay. Each geode robot costs 4 ore and 8 obsidian.
 TEST_DATA
     my $data = read_file("input-19");
     return prepare_data($data);
@@ -52,8 +51,8 @@ sub sum($items, $acc) {
 }
 
 sub part_1(@data) {
-#return sum(get_quality_levels([@data],[]), 0);
-    return 0;
+    return sum(get_quality_levels([@data],[]), 0);
+    #return 0;
 }
 
 sub get_quality_levels($blueprints, $ret ) {
@@ -74,7 +73,7 @@ sub get_quality_levels($blueprints, $ret ) {
         },
     };
     say $blueprint->{id};
-    push @$ret, get_quality_level($blueprint, [ [24, $initial_state, [] ] ], {}) * $blueprint->{id};
+    push @$ret, get_quality_level($blueprint, [ [ 24, $initial_state, [] ] ], {}) * $blueprint->{id};
     goto &get_quality_levels;
 }
 
@@ -107,67 +106,108 @@ sub could_build_three_last_time($blueprint, $state) {
         && $clay_last_time >= $blueprint->{bots}[1]{needs}{clay};
 }
 
+sub get_moves_to_build($state, $bp, $t) {
+    return -1 if grep { $state->{bots}{$_} == 0 } keys $bp->{needs}->%*;
+    my @needs = keys $bp->{needs}->%*;
+    my @res = map { $state->{resources}{$_} - $bp->{needs}{$_} } @needs;
+    if ( scalar @res == scalar grep { $_ >= 0 } @res ) {
+      return $t;
+    }
+    #@res = map { $res[$_] + $state->{bots}->{$needs[$_]} } 0..$#needs;
+    return try_to_build_it_tho(\@res, $state->{bots}, \@needs, $t - 1);
+}
+
+sub try_to_build_it_tho($res, $bots, $needs, $t) {
+    return $t if $t < 0;
+    $res = [map { $res->[$_] + $bots->{$needs->[$_]} } 0..$#$needs];
+    if ( scalar @$res == scalar grep { $_ >= 0 } @$res ) { 
+      return $t;
+    }
+    @_ = ($res, $bots, $needs, $t - 1);
+    goto &try_to_build_it_tho;
+}
+
+sub can_build_more_ore_bots($bots, $blueprint) {
+    my $ok = 0;
+    $ok ||= $bots < $_->{needs}{ore} for $blueprint->{bots}->@*;
+    return $ok;
+}
+
+sub can_build_more_clay_bots($bots, $blueprint) {
+    return $bots < $blueprint->{bots}[1]{needs}{clay};
+}
+
+sub can_build_more_obsidian_bots($bots, $blueprint) {
+    return $bots < $blueprint->{bots}[0]{needs}{obsidian};
+}
+
+sub max_possible($state, $time_left) {
+    my $curr = $state->{resources}{geode};
+    my $curr_bots = $state->{bots}{geode};
+    return $curr + $curr_bots * $time_left + $time_left*($time_left-1)/2;
+}
+
 sub get_quality_level($blueprint, $deq, $max) {
     return $max->{0}{geode} unless @$deq;
-    my ($time_left, $state, $path) = (pop @$deq)->@*;
+    my ($time_left, $state, $path) = (shift @$deq)->@*;
 
-    $max->{$time_left} //= {
+    $max->{0} //= {
         geode => 0,
     };
-    if ( ( $time_left > 0 ) && ( $state->{resources}{geode} >= $max->{$time_left}{geode} ) ) {
+    if ( ($time_left > 0) && ( max_possible($state, $time_left) > $max->{0}{geode} ) ) {
         my @new_states = 
         map {
-            my $ns = $_;
+            my ($t_left, $ns) = $_->@*;
+            #say $t_left if $time_left == 21;
+            #say $time_left - $t_left if $time_left == 21;
+            #say Dumper($ns) if $time_left == 21;
+            [ $t_left - 1,
             {
                 bots => $ns->{bots},
                 resources => {
-                    map { $_ => ($ns->{resources}{$_} // 0) + $state->{bots}{$_} } keys $state->{bots}->%*
+                    map { $_ => ($ns->{resources}{$_} // 0) + ($state->{bots}{$_} * ($time_left - $t_left + 1)) } keys $state->{bots}->%*
                 },
                 made => made_to_letter($ns->{made})
             }
+            ]
         } grep {
-            ! grep { $_ < 0 } values $_->{resources}->%*
+            $_->[0] > 0
         } map {
             my $bp = $_;
             my %bots = $state->{bots}->%*;
             my %resources = $state->{resources}->%*;
+            my $t_when_built = get_moves_to_build($state, $bp, $time_left);
             $bots{$bp->{makes}}++;
             $resources{$_} -= $bp->{needs}{$_} for keys $bp->{needs}->%*;
+            [ $t_when_built,
             {
                 bots => { %bots },
                 resources => { %resources },
                 made => $bp->{makes},
-            }
+            } ]
+        } grep {
+            $_->{makes} eq 'geode'
+            || $_->{makes} eq 'ore' && can_build_more_ore_bots( $state->{bots}{ore}, $blueprint )
+            || $_->{makes} eq 'clay' && can_build_more_clay_bots( $state->{bots}{clay}, $blueprint )
+            || $_->{makes} eq 'obsidian' && can_build_more_obsidian_bots( $state->{bots}{obsidian}, $blueprint) 
         } $blueprint->{bots}->@*;
 
-        my $waiting_too_long_to_start = scalar(grep {$_->{made} eq 'o' || $_->{made} eq 'c' } @new_states) == 2 && $state->{bots}{clay} == 0 && $state->{bots}{ore} == 1;
-        my $waiting_too_long = scalar(grep {$_->{made} eq 'o' || $_->{made} eq 'c' } @new_states) == 2 && $state->{bots}{clay} == 0;  
 
-        if ( scalar(grep {$_->{made} eq 'o' || $_->{made} eq 'b' || $_->{made} eq 'c' } @new_states)  == 3 && $path->[-1] eq '-' && could_build_three_last_time($blueprint, $state) ) {
-            @new_states = grep { $_->{made} ne 'o' || $_->{made} ne 'b' || $_->{made} ne 'c'  } @new_states;
-        }
-
-
-        if ( scalar(grep {$_->{made} eq 'o' || $_->{made} eq 'c' } @new_states) == 2 && $path->[-1] eq "-" && could_build_both_last_time($blueprint, $state) ) {
-            @new_states = grep { $_->{made} ne 'o' && $_->{made} ne 'c' } @new_states;
-        }
-
-        push @new_states, {
+        push @new_states, [0, {
             bots => {
                 $state->{bots}->%*
             },
             resources => {
-                map { $_ => ($state->{resources}{$_} // 0) + $state->{bots}{$_} } keys $state->{bots}->%*
+                map { $_ => ($state->{resources}{$_} // 0) + $state->{bots}{$_} * $time_left } keys $state->{bots}->%*
             },
             made => '-'
-        } unless $waiting_too_long_to_start || $waiting_too_long;
+        }];
 
-        my @to_build = grep { $_->{made} eq 'g' } @new_states;
-        @to_build = @new_states unless scalar @to_build;
 
-        push @$deq, [$time_left - 1, $_, [@$path, $_->{made}] ] for reverse @to_build; # sort { calc_score($blueprint, $time_left - 1, $b) <=> calc_score($blueprint, $time_left - 1, $a) } @new_states;
+        push @$deq, [$_->[0], $_->[1], [@$path, '-'x( $time_left - $_->[0] -1), $_->[1]{made}] ] for @new_states;
+
     }
-    if ($state->{resources}{geode} > $max->{$time_left}{geode} ) {
+    if ( $time_left == 0 && $state->{resources}{geode} > $max->{$time_left}{geode} ) {
         say "new best: $state->{resources}{geode} $time_left";
         say join "", $path->@*;
         $max->{$time_left}{geode} = $state->{resources}{geode};
