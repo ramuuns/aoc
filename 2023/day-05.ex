a57b38822f1cde043cdc1f4835b514b914dc702b
Ramūns Usovs <ramuuns@enkurs.org>
2023-12-05 12:33:31 +0100

    Format and get rid of giant cond block

--- 2023/day-05.ex
+++ 2023/day-05.ex
@@ -62,7 +62,7 @@ humidity-to-location map:
 
   def prepare_data(data) do
     [seeds, _ | maps] = data
-    { seeds |> parse_seeds, maps |> parse_maps([], :parse_header) }
+    {seeds |> parse_seeds, maps |> parse_maps([], :parse_header)}
   end
 
   def parse_seeds("seeds: " <> seeds) do
@@ -71,26 +71,29 @@ humidity-to-location map:
 
   def parse_maps([], res, _), do: res |> Enum.reverse()
   def parse_maps(["" | tail], res, _), do: parse_maps(tail, res, :parse_header)
+
   def parse_maps([header | rest], res, :parse_header) do
     [ft, _] = String.split(header, " ", trim: true)
     [from, _, to] = String.split(ft, "-", trim: true)
 
-    parse_maps(rest, [{from, to, [] } | res], :parse_numbers)
+    parse_maps(rest, [{from, to, []} | res], :parse_numbers)
   end
 
-  def parse_maps([nrs | rest], [{from,to, list} | res], :parse_numbers) do
-    [dest, src, range] = nrs |> String.split(~r"\s+", trim: true) |> Enum.map(&String.to_integer/1)
-    parse_maps(rest,[{from, to, [{src, dest, range} | list]} | res], :parse_numbers)
+  def parse_maps([nrs | rest], [{from, to, list} | res], :parse_numbers) do
+    [dest, src, range] =
+      nrs |> String.split(~r"\s+", trim: true) |> Enum.map(&String.to_integer/1)
+
+    parse_maps(rest, [{from, to, [{src, dest, range} | list]} | res], :parse_numbers)
   end
 
   def part1(data) do
-    {seeds, maps} = data 
+    {seeds, maps} = data
     find_min_location(seeds, maps)
   end
 
   def part2(data) do
-    {seeds, maps} = data 
-    find_min_location_ranges(seeds,maps)
+    {seeds, maps} = data
+    find_min_location_ranges(seeds, maps)
   end
 
   def find_min_location(seeds, maps) do
@@ -100,14 +103,20 @@ humidity-to-location map:
   end
 
   def find_location(num, []), do: num
-  def find_location(num, [{_,_, mappings} | rest]), do: find_location(map_number(num, mappings), rest)
+
+  def find_location(num, [{_, _, mappings} | rest]),
+    do: find_location(map_number(num, mappings), rest)
 
   def map_number(num, []), do: num
-  def map_number(num, [{src, dest, range} | _]) when num >= src and num < src + range, do: num - src + dest
-  def map_number(num, [_|rest]), do: map_number(num, rest)
+
+  def map_number(num, [{src, dest, range} | _]) when num >= src and num < src + range,
+    do: num - src + dest
+
+  def map_number(num, [_ | rest]), do: map_number(num, rest)
 
   def find_min_location_ranges(seeds, maps) do
-    seeds |> Enum.reduce([], fn 
+    seeds
+    |> Enum.reduce([], fn
       i, [{num, nil} | rest] -> [{num, i} | rest]
       i, rest -> [{i, nil} | rest]
     end)
@@ -118,44 +127,59 @@ humidity-to-location map:
   def find_location_min_range(ranges, []) do
     ranges |> Enum.map(fn {n, _} -> n end) |> Enum.min()
   end
-  def find_location_min_range(rages, [{_,_, mappings} | rest]) do
+
+  def find_location_min_range(rages, [{_, _, mappings} | rest]) do
     find_location_min_range(map_ranges(rages, mappings, []), rest)
   end
 
   def map_ranges([], _, res), do: res
+
   def map_ranges([item | rest], mappings, res) do
-    map_ranges(rest, mappings, range_to_next_ranges(item, mappings) ++ res) 
+    map_ranges(rest, mappings, range_to_next_ranges(item, mappings) ++ res)
   end
 
   def range_to_next_ranges({start, range}, mappings) do
-    st = map_number(start, mappings)
-    r_end = map_number(start+range, mappings)
-    if r_end - st == range do
-      # no split needed
-      [{st, range}] 
-    else
-      range_to_ranges([{start, range}], mappings, mappings, [])
-    end
+    range_to_ranges([{start, range}], mappings, mappings, [])
   end
-
 
   def range_to_ranges([], _, _, res), do: res
-  def range_to_ranges([item | src_ranges], [], mappings, res), do: range_to_ranges(src_ranges, mappings, mappings, [ item | res ])
-  def range_to_ranges([{start, range} | src_ranges], [{src, dst, sd_range} | rest], mappings, res) do
-    cond do
-      start >= src and start < src + sd_range and  start + range < src + sd_range ->
-        range_to_ranges(src_ranges, mappings, mappings, [ { start - src + dst, range} | res])
-      start >= src and start < src + sd_range ->
-        st = start - src + dst
-        rng = (src + sd_range) - start
-        range_to_ranges([{ start + rng, range - rng } | src_ranges], mappings, mappings, [ {st, rng} | res ])
-      src > start and start + range > src and start + range > src + sd_range ->
-        range_to_ranges([{ start, src - start }, { src + sd_range, start + range - (src + sd_range) } | src_ranges ],
-          mappings, mappings, [{ dst, sd_range } | res ])
-      src > start and start + range > src ->
-        range_to_ranges([{ start, src - start} | src_ranges], mappings, mappings, [{dst, range - (src - start)} | res])
-      true -> range_to_ranges([{start, range} | src_ranges], rest, mappings, res)
-    end 
-  end
 
+  def range_to_ranges([item | src_ranges], [], mappings, res),
+    do: range_to_ranges(src_ranges, mappings, mappings, [item | res])
+
+  def range_to_ranges([{start, range} | src_ranges], [{src, dst, sd_range} | _], mappings, res)
+      when start >= src and start < src + sd_range and start + range < src + sd_range,
+      do: range_to_ranges(src_ranges, mappings, mappings, [{start - src + dst, range} | res])
+
+  def range_to_ranges([{start, range} | src_ranges], [{src, dst, sd_range} | _], mappings, res)
+      when start >= src and start < src + sd_range,
+      do:
+        range_to_ranges(
+          [{start + (src + sd_range - start), range - (src + sd_range - start)} | src_ranges],
+          mappings,
+          mappings,
+          [
+            {start - src + dst, src + sd_range - start} | res
+          ]
+        )
+
+  def range_to_ranges([{start, range} | src_ranges], [{src, dst, sd_range} | _], mappings, res)
+      when src > start and start + range > src and start + range > src + sd_range,
+      do:
+        range_to_ranges(
+          [{start, src - start}, {src + sd_range, start + range - (src + sd_range)} | src_ranges],
+          mappings,
+          mappings,
+          [{dst, sd_range} | res]
+        )
+
+  def range_to_ranges([{start, range} | src_ranges], [{src, dst, _} | _], mappings, res)
+      when src > start and start + range > src,
+      do:
+        range_to_ranges([{start, src - start} | src_ranges], mappings, mappings, [
+          {dst, range - (src - start)} | res
+        ])
+
+  def range_to_ranges(source, [_ | rest], mappings, res),
+    do: range_to_ranges(source, rest, mappings, res)
 end
