54d7946e0af18281c4381a3ba22029b4d1dfef25
Ramūns Usovs <ramuuns@ramuuns.com>
2024-12-02 12:53:44 +0200

    day2 now in O(n)

--- 2024/day02.py
+++ 2024/day02.py
@@ -24,10 +24,22 @@ def safe(arr):
 def damp_safe(arr):
     if safe(arr):
         return True
-    for idx, _el in enumerate(arr):
-        if safe(arr[0:idx] + arr[idx+1:]):
-            return True
-    return False
+    if safe(arr[1:]):
+        return True
+    return damp_safe_inner(arr) or damp_safe_inner(arr[::-1])
+
+def damp_safe_inner(arr):
+    is_increasing = arr[-1] > arr[0]
+    dampened = False
+    p = arr[0]
+    for el in arr[1:]:
+        if (is_increasing and el <= p) or (not is_increasing and el >= p) or abs(el - p) > 3:
+            if dampened:
+                return False
+            dampened = True
+            continue
+        p = el
+    return True
 
 def test():
     test_data = """
@@ -41,3 +53,5 @@ def test():
     assert part1(parse_data(test_data)) == 2, "Should be 2"
     assert part2(parse_data(test_data)) == 4, "Should be 4"
     assert damp_safe([1,5,6,7,8]), "should be safe"
+    assert damp_safe([93, 90, 92, 90, 89, 87, 84, 81]), "should be safe"
+    assert damp_safe([14, 13, 11, 12, 8]), "should also be safe"
