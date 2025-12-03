b07cbb68d91ac0e735c81b3b568fb4a7b33eff36
Ramūns Usovs <ramuuns@ramuuns.com>
2025-12-03 12:07:39 +0200

    Day 3

---
+++ 2025/day03.py
@@ -0,0 +1,55 @@
+
+def run(data):
+    data1 = parse_data(data)
+    data2 = parse_data(data)
+    return (part1(data1), part2(data2))
+
+def parse_data(data):
+    return [list(row) for row in  data.split("\n")]
+
+def part1(data):
+    return sum([max_joltage(battery) for battery in data ])
+
+def part2(data):
+    return sum([max_joltage_12(battery) for battery in data])
+
+def max_joltage(row):
+    num_row = [int(d) for d in row]
+    tens = 0
+    singles = 0
+    max_idx_tens = len(num_row) - 1
+    for idx, n in enumerate(num_row):
+        if n > tens and idx != max_idx_tens:
+            tens = n
+            singles = 0
+        elif n > singles:
+            singles = n
+        if tens == 9 and singles == 9:
+            break
+    return tens*10 + singles
+
+def max_joltage_12(row):
+    num_row = [int(d) for d in row]
+    max_digits = [0,0,0,0,0,0,0,0,0,0,0,0]
+    max_indexes = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    max_l = len(row)
+    for idx, n in enumerate(num_row):
+        for k, d in enumerate(max_digits):
+            if n > d and idx < max_l - max_indexes[k]:
+                max_digits[k] = n
+                for dx in range(k+1, 12):
+                    max_digits[dx] = 0
+
+                break
+    s = 0
+    for d in max_digits:
+        s = s*10 + d
+    return s
+
+def test():
+    test_data = """987654321111111
+811111111111119
+234234234234278
+818181911112111"""
+    assert part1(parse_data(test_data)) == 357, "Should be 357"
+    assert part2(parse_data(test_data)) == 3121910778619, "Should be 3121910778619"
